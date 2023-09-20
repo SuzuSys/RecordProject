b26f3d7d63e81bdb4d52e0d086e5a6cc5f60127b
SuzuSys <sigmatics@outlook.jp>
2023-09-20 15:31:59 +0900

    updated RecordFuntionTest

--- record-amplify/amplify/backend/function/RecordFunctionTest/src/index.py
+++ record-amplify/amplify/backend/function/RecordFunctionTest/src/index.py
@@ -1,6 +1,31 @@
 import json
+import boto3
+from time import time
+
+client = boto3.client('timestream-write')
 
 def handler(event, context):
+  response = client.write_records(
+    DatabaseName='record-suzusys',
+    TableName='cycle',
+    Records=[
+      {
+        'Dimensions': [
+            {
+                'Name': 'forcibly',
+                'Value': 'false'
+            }
+        ],
+        'MeasureName': 'action',
+        'MeasureValue': 'meal',
+        'MeasureValueType': 'VARCHAR',
+        'Time': str(int(time())),
+        'TimeUnit': 'SECONDS'
+      }
+    ]
+  )
+  print('response')
+  print(response)
   print('received event:')
   print(event)
   
