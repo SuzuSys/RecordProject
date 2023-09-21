b8f5322dd5248a46668ec5c5f5cd923bc6c00688
SuzuSys <sigmatics@outlook.jp>
2023-09-22 01:40:27 +0900

    added account setting page

--- record-amplify/src/main.ts
+++ record-amplify/src/main.ts
@@ -3,10 +3,12 @@ import AuthContainer from './AuthContainer.vue'
 import router from '@/router'
 import vuetify from '@/plugins/vuetify'
 import { loadFonts } from '@/plugins/webfontloader'
+import { createPinia } from 'pinia';
 
 loadFonts()
 
 createApp(AuthContainer)
   .use(router)
   .use(vuetify)
+  .use(createPinia())
   .mount('#app')
--- record-amplify/src/router/index.ts
+++ record-amplify/src/router/index.ts
@@ -1,16 +1,21 @@
-import { createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory} from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      redirect: { name: "DashboardView" },
+      path: '/',
+      redirect: { name: 'DashboardView' },
     },
     {
-      path: "/dashboard",
-      name: "DashboardView",
-      component: () => import("../views/DashboardView.vue"),
+      path: '/dashboard',
+      name: 'DashboardView',
+      component: () => import('@/views/DashboardView.vue'),
+    },
+    {
+      path: '/account',
+      name: 'AccountView',
+      component: () => import('@/views/AccountView.vue'),
     },
   ],
 });
