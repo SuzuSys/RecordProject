88e9355fbef42580fcc33b1b6b23bf9eaffe3f7a
SuzuSys <sigmatics@outlook.jp>
2023-09-29 15:36:23 +0900

    added iOS/setup.py

--- iOS/iOS_setup.sh
+++ iOS/iOS_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
+curl -L https://raw.githubusercontent.com/SuzuSys/RecordProject/main/iOS/setup.py -o setup.py
 curl -L https://raw.githubusercontent.com/SuzuSys/RecordProject/main/iOS/API.py -o API.py
 curl -L https://raw.githubusercontent.com/SuzuSys/RecordProject/main/iOS/requirements.txt -o requirements.txt
 pip install -r requirements.txt
-
+python setup.py $1
