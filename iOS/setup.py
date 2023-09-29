503c152237de3bd1cd6729703ba5ae44729cc05b
SuzuSys <sigmatics@outlook.jp>
2023-09-29 21:05:15 +0900

    fixed setup.py

--- iOS/setup.py
+++ iOS/setup.py
@@ -2,14 +2,39 @@ import sys
 import re
 import json
 import yaml
+import boto3
+from botocore.config import Config
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
-import pprint
+from pprint import pprint
 
-# Set up configuration
-regex = re.compile(r'^RecordConfig:')
-config_json = regex.sub('', sys.argv[1])
-config_dict = json.loads(config_json)
+# get config
+try:
+  regex = re.compile(r'^RecordConfig:')
+  config_json = regex.sub('', sys.argv[1])
+  config_dict = json.loads(config_json)
+except Exception as e:
+  exit(e)
+
+# get access token
+client = boto3.client(
+  'cognito-idp',
+  config=Config(region_name=config_dict['project_region'], signature_version='v4')
+)
+try:
+  response = client.initiate_auth(
+    AuthFlow='REFRESH_TOKEN',
+    ClientId=config_dict['user_pools_web_client_id'],
+    AuthParameters={
+      'REFRESH_TOKEN': config_dict['refresh_token'],
+    },
+  )
+  print('---------------- access token response ---------------')
+  pprint(response)
+except client.exceptions.NotAuthorizedException as e:
+  exit(e)
+
+# save configuration
 with open('credentials.yaml', 'w') as f:
   yaml.dump(config_dict, f, default_flow_style=False, allow_unicode=True)
 
@@ -30,7 +55,7 @@ query = '''
     verifySetup
   }
 '''
-response = client.execute(gql(query))
-pprint.pprint(response)
-print()
-print('Application setup was completed!')
+# response = client.execute(gql(query))
+# pprint(response)
+# print()
+# print('Application setup was completed!')
