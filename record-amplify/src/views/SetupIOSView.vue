af56e57ab56b6ac1f33d0eadfc2699cff401b97b
SuzuSys <sigmatics@outlook.jp>
2023-09-29 11:22:03 +0900

    added SetupIOSView.vue

--- record-amplify/src/views/SetupIOSView.vue
+++ record-amplify/src/views/SetupIOSView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
+import awsconfig from "@/aws-exports";
 import { reactive } from 'vue';
 import { Auth } from 'aws-amplify';
 
 interface State {
-  refresh_token: string;
+  setup_config_str: string;
   show: {
     alert: {
       success: {
@@ -12,13 +13,13 @@ interface State {
       error: {
         copy: boolean;
         get_token: boolean;
-      }
+      };
     };
   };
 };
 
 const state: State = reactive<State>({
-  refresh_token: '',
+  setup_config_str: '',
   show: {
     alert: {
       success: {
@@ -32,24 +33,39 @@ const state: State = reactive<State>({
   },
 });
 
-getRefreshToken();
+getSetupInfo();
 
 // methods setting
-async function getRefreshToken() {
+async function getSetupInfo() {
   try {
     const session = await Auth.currentSession();
-    state.refresh_token = session.getRefreshToken().getToken();
+    const refresh_token = session.getRefreshToken().getToken();
+    const {
+      aws_project_region,
+      aws_user_pools_id,
+      aws_user_pools_web_client_id,
+      aws_appsync_graphqlEndpoint,
+    } = awsconfig;
+    const setup_config = {
+      project_region: aws_project_region,
+      user_pools_id: aws_user_pools_id,
+      user_pools_web_client_id: aws_user_pools_web_client_id,
+      appsync_graphqlEndpoint: aws_appsync_graphqlEndpoint,
+      refresh_token,
+    };
+    state.setup_config_str = 'RecordConfig:' + JSON.stringify(setup_config);
+    console.log(state.setup_config_str);
   } catch (e) {
     console.error(e);
     state.show.alert.success.copy = false;
     state.show.alert.error.copy = false;
     state.show.alert.error.get_token = true;
   }
-  
-}
+};
+
 async function copy() {
   if (navigator.clipboard) {
-    await navigator.clipboard.writeText('RefreshToken:' + state.refresh_token);
+    await navigator.clipboard.writeText('RecordConfig:' + state.setup_config_str);
     state.show.alert.success.copy = true;
     state.show.alert.error.copy = false;
     state.show.alert.error.get_token = false;
@@ -58,27 +74,24 @@ async function copy() {
     state.show.alert.error.copy = true;
     state.show.alert.error.get_token = false;
   }
-}
-
-
-
+};
 </script>
 
 <template>
   <v-card class="mx-auto my-12" max-width="500" variant="outlined">
-    <v-card-title>Recovery iOS Application</v-card-title>
+    <v-card-title>Setup iOS Application</v-card-title>
     <v-card-actions>
       <v-container class="pa-1">
         <v-banner>
           <v-banner-text>
-            Refresh token の有効期限が切れました．Refresh token をクリップボードにコピーし，再度ショートカットで同様の操作を行うことによってシステムを回復することができます．
+            iOS デバイスのセットアップを行います．iOS Config をクリップボードにコピーし，再度同様のショートカットにアクセスしてください．
           </v-banner-text>
           <template v-slot:actions>
             <v-btn
               @click="copy"
               variant="outlined"
             >
-              Copy refresh token
+              Copy iOS config
             </v-btn>
           </template>
         </v-banner>
@@ -89,7 +102,7 @@ async function copy() {
           class="my-2"
           variant="outlined"
         >
-          Refresh token がコピーされました．
+          iOS config がコピーされました．
         </v-alert>
         <v-alert
           type="error"
@@ -107,7 +120,7 @@ async function copy() {
           class="my-2"
           variant="outlined"
         >
-          Refresh token の取得に失敗しました．
+          iOS config の取得に失敗しました．
         </v-alert>
       </v-container>
     </v-card-actions>
