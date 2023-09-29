6e2d2be06a1e282bfb3f4cabc2d36ca9738b070b
SuzuSys <sigmatics@outlook.jp>
2023-09-29 17:33:35 +0900

    updated setup.py

--- iOS/setup.py
+++ iOS/setup.py
@@ -9,3 +9,5 @@ print(config_json)
 config_dict = json.loads(config_json)
 with open('credentials.yaml', 'w') as f:
     yaml.dump(config_dict, f, default_flow_style=False, allow_unicode=True)
+    print()
+    print('Application setup was completed!')
