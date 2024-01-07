c5b277f94fe1b3eb40a24ed28b54f92f71b76c80
SuzuSys <sigmatics@outlook.jp>
2024-01-07 11:20:04 +0900

    create RecordFunctionTestRDS

--- record-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ record-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -22,6 +22,13 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     },
+    "RecordFunctionTestRDS": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "RecordFunctionVerifySetup": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
